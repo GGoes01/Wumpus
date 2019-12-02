62c47250c5d02f71486d4a5c6acd2e4e69b48201
GGoes01 <34575335+GGoes01@users.noreply.github.com>
2019-12-02 08:54:03 -0300

    02/12/2019 - 08:52
    Anda: o
    Poço: x
    Ouro: x
    Wumpus: x
    Retorna: x

---
+++ Populacao.py
@@ -0,0 +1,30 @@
+from Agente import Agente
+from Fitness import Fitness as fit
+import random
+
+
+class Populacao:
+    def __init__(self, quantidade, geracao):
+        self.__quantidade = quantidade
+        self.__geracao = geracao
+
+    def gerar_populacao(self):
+        vetor_populacao = []
+
+        for contador in range(self.__quantidade):
+            celula = Agente(random.randint(0, 64))
+            celula.set_movimento(celula.gerar_movimento())
+
+            fitness, pontuacao = celula.pecado_inicial(celula)
+
+            celula.set_pontucao(pontuacao)
+            celula.set_fitness(fitness)
+
+            vetor_populacao.append(celula)
+
+            for cel in vetor_populacao:
+                indice = vetor_populacao.index(cel)
+                id = str(self.__geracao) + '_00_' + str(indice)
+                cel.set_id(id)
+
+        return vetor_populacao
