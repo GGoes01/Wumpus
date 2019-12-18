2269c29564f07729379876d506492b73cb554d95
GGoes01 <34575335+GGoes01@users.noreply.github.com>
2019-12-18 01:53:36 -0300

    Create main.py

---
+++ V18.12.19_01.51/main.py
@@ -0,0 +1,11 @@
+from AmbienteBruto import Ambiente
+from Agente import Agente
+from Mundo_de_Wumpus import Jogo
+import random
+from Casa import Casa
+from Populacao import Populacao
+from Memoria import Memoria as mem
+from Fitness import Fitness as fit
+
+jogo = Jogo(1500, 4)
+dados = jogo.executar()
